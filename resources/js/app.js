59ab1adde78d7eedce9a075b828ed6d3c46b3100
Alex <alexey.a.volkov@pm.me>
2020-04-25 18:03:12 +0300

    Vue Started

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
-require('./bootstrap');
+import Vue from "vue";
+import router from "./router";
+
+// Vue.config.productionTip = false;
+
+new Vue({
+    router
+}).$mount("#app");
---
+++ resources/js/components/About.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Alex</h1>
+        <p>My age is 26</p>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
